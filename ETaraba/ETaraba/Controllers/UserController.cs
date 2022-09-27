8ea3363ec8954a1e9b720f72aef8e1476e286d04
larisaoprea1 <103680895+larisaoprea1@users.noreply.github.com>
2022-09-27 16:32:50 +0300

    register

---
+++ ETaraba/ETaraba/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using ETaraba.Domain.Models;
+using ETaraba.DTOs.UserDTOs;
+using ETaraba.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETaraba.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<UserRole> _roleManager;
+        private readonly IMapper _mapper;
+        public UserController(UserManager<User> userManager, RoleManager<UserRole> roleManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _mapper = mapper;   
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUserByUserName(string username)
+        {
+            var userToFind = await _userManager.FindByNameAsync(username);
+            return Ok(_mapper.Map<UserDTO>(userToFind));
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register( [FromBody] UserForCreationDTO user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var userExist = await _userManager.FindByNameAsync(user.UserName);
+            if(userExist != null)
+            {
+                return BadRequest("User already exists");
+            }
+            var usertToCreate = new User
+            {
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                ProfileImgSrc = user.ProfileImageSrc
+
+            };
+            var result = await _userManager.CreateAsync(usertToCreate, user.Password);
+            var userToReturn = _mapper.Map<UserDTO>(usertToCreate);
+            if (!result.Succeeded)
+            {
+                return BadRequest("Failed to create user");
+            }        
+            return Ok(userToReturn);
+        }
+    }
+}
---
+++ ETaraba/ETaraba/Profiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ETaraba.Domain.Models;
+using ETaraba.DTOs.UserDTOs;
+
+namespace ETaraba.Profiles
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+            CreateMap<UserForCreationDTO, User>();
+        }
+    }
+}
